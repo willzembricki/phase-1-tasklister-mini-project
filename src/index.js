b3b6d12472dfc17266fa6960c74057e1ba833e45
Gedler <gedler.jeanpierre@stonybrook.edu>
2021-03-17 16:42:58 -0400

    Progress

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ let commentForm = document.querySelector("form#create-task-form")
 let taskList = document.querySelector("ul#tasks")
 
 
-
-
 commentForm.addEventListener("submit", function(evt){
   // to pause the refresh
   evt.preventDefault()
@@ -12,7 +10,19 @@ commentForm.addEventListener("submit", function(evt){
   let theirTask = theirTaskInput.value
   // console.log(theirTask)
   // create a list for it and then add it to the list 
-  let blankLi = document.createElement("li")
-  blankLi.innerText = theirTask
+  let blankLi = document.createElement("li") 
+  let blankSpan = document.createElement('span')
+  let buttonNode = document.createElement('button')
+
+  blankSpan.innerText = theirTask
   taskList.append(blankLi)
+  blankLi.appendChild(blankSpan)
+  blankLi.appendChild(buttonNode)
+  buttonNode.innerText = 'delete'
+  console.log(taskList)
 })
+
+buttonNode.addEventListener("click", function (evt){
+         
+})
+
