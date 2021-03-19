748f99f3a2af152362569ee0548060a7ce3d3723
William Zembricki <wzembricki@gmail.com>
2021-03-19 14:55:51 -0400

    Will driving main task finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let commentForm = document.querySelector("form#create-task-form")
+let taskList = document.querySelector("ul#tasks")
+
+
+
+
+commentForm.addEventListener("submit", function(evt){
+  // to pause the refresh
+  evt.preventDefault()
+  // get their input and store it
+  let theirTaskInput = commentForm["new-task-description"]
+  let theirTask = theirTaskInput.value
+  // console.log(theirTask)
+  // create a list for it and then add it to the list 
+  let blankLi = document.createElement("li")
+  blankLi.innerText = theirTask
+  taskList.append(blankLi)
+})
